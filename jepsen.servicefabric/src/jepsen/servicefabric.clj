66a442f27a8dd17d4e90f8b2e31de776b387442c
Doommius <Mark@jervelun.com>
2021-06-14 18:38:28 +0200

    Merging repos

--- jepsen.servicefabric/src/jepsen/servicefabric.clj
+++ jepsen.servicefabric/src/jepsen/servicefabric.clj
@@ -12,23 +12,20 @@
   "Given an options map from the command line runner (e.g. :nodes, :ssh,
   :concurrency, ...), constructs a test map."
   [opts]
- (merge tests/noop-test
+  (merge tests/noop-test
          opts
          {:name "service fabric"
           :os   debian/os
           :db   (db "v8.0.513.1804")
           :pure-generators true}))
-
 (defn db
   "servicefabric DB for a particular version."
   [version]
   (reify db/DB
     (setup! [_ test node]
       (info node "installing servicefabric" version))
-
     (teardown! [_ test node]
       (info node "tearing down servicefabric"))))
-
 (defn -main
   "Handles command line arguments. Can either run a test, or a web server for
   browsing results."
