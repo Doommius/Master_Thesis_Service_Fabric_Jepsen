51993610dc7f1d8102932a691250bea895f2c88d
Doommius <Mark@jervelun.com>
2021-07-01 21:07:43 +0200

    Service works on Azure cluster now.

---
+++ Jepsen/Jepsen/Scripts/forceremove.ps1
@@ -0,0 +1,33 @@
+﻿$ClusterName= "jepsen.northeurope.cloudapp.azure.com:19080"
+
+$Certthumprint = "E58AE7E7077522C401927883CFC3E610411134B1"
+
+Connect-ServiceFabricCluster -ConnectionEndpoint $ClusterName -KeepAliveIntervalInSec 10 `
+
+     -X509Credential `
+
+     -ServerCertThumbprint $Certthumprint  `
+
+     -FindType FindByThumbprint `
+
+     -FindValue $Certthumprint `
+
+     -StoreLocation CurrentUser `
+
+     -StoreName My
+
+$ApplicationName = "fabric:/Jepsen"
+
+ 
+
+foreach($node in Get-ServiceFabricNode)
+
+{  
+
+ [void](Get-ServiceFabricDeployedReplica -NodeName $node.NodeName -ApplicationName $ApplicationName | Remove-ServiceFabricReplica -NodeName $node.NodeName -ForceRemove)
+
+}
+
+Remove-ServiceFabricApplication -ApplicationName $ApplicationName -Force
+
+ 
