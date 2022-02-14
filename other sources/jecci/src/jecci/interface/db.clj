a9eebacde201025fff3348b1873626ef0b5d2d7d
Mark Jervelund <Doommius@users.noreply.github.com>
2022-02-14 20:05:37 +0100

    Removed non project files

--- other sources/jecci/src/jecci/interface/db.clj
+++
@@ -1,4 +0,0 @@
-(ns jecci.interface.db
-  (:require [jecci.interface.resolver :as r]))
-
-(def db (r/resolve! "db" "db"))
--- other sources/jepsen-main/disque/project.clj
+++
@@ -1,9 +0,0 @@
-(defproject jepsen.disque "0.1.0-SNAPSHOT"
-  :description "Jepsen test for Disque"
-  :url "https://github.com/aphyr/jepsen"
-  :license {:name "Eclipse Public License"
-            :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.6.0"]
-                 [jepsen "0.1.17"]
-                 [com.github.xetorthio/jedisque "0.0.4"]]
-  :main jepsen.disque)
